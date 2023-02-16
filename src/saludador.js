8a3e0bce3299141ee0dc2561b9d66f0d17fd1f8a
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-02-16 09:59:23 -0400

    Se agrego la funcionalidad de saludar por edad

--- src/saludador.js
+++ src/saludador.js
@@ -1,18 +1,21 @@
-
-
 const nombre = document.querySelector("#nombre");
-const genero=document.querySelector("#genero")
+const genero=document.querySelector("#genero");
+const edad=document.querySelector("#edad");
 const form = document.querySelector("#saludador-form");
 const div = document.querySelector("#resultado-div");
-
-
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   let gen;
   if(genero.value=="hombre")
-    gen="Sr. ";
+    if(edad.value>30)
+      gen="Sr. ";
+    else 
+      gen="Joven ";
   else
-    gen="Sra. ";
+    if(edad.value>30)
+      gen="Sra. ";
+    else
+      gen="Srta. ";
   div.innerHTML = "<p> hola " + gen + nombre.value + "</p>";
 });
