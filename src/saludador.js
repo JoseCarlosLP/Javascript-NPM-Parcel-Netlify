58eb6ff36592583d96bbef54ef0a739787f11566
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-02-16 09:49:44 -0400

    Se agrego la funcionalidad de saludar por genero

--- src/saludador.js
+++ src/saludador.js
@@ -1,13 +1,18 @@
 
 
 const nombre = document.querySelector("#nombre");
-
+const genero=document.querySelector("#genero")
 const form = document.querySelector("#saludador-form");
 const div = document.querySelector("#resultado-div");
 
 
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  div.innerHTML = "<p> hola " + nombre.value + "</p>";
+  let gen;
+  if(genero.value=="hombre")
+    gen="Sr. ";
+  else
+    gen="Sra. ";
+  div.innerHTML = "<p> hola " + gen + nombre.value + "</p>";
 });
