75a258cba418457ef0218f1e3649648a27191116
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-02-17 13:37:27 -0400

    Version con funcionalidad implementada de saludo tomando en cuenta la hora del dia

--- src/saludador.js
+++ src/saludador.js
@@ -6,16 +6,25 @@ const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  let fechaActual = new Date();
+  let horaActual = fechaActual.getHours();
   let gen;
-  if(genero.value=="Hombre")
-    if(edad.value>30)
-      gen="Sr. ";
+  let saludoXHora;
+  if(horaActual>=6 && horaActual <12)
+    saludoXHora="buenos dias";
+  else if(horaActual>=12 && horaActual<19)
+    saludoXHora="buenas tardes";
     else 
+    saludoXHora="buenas noches";
+  if(genero.value=="Hombre")
+    if(edad.value<=30)
       gen="Joven ";
+    else 
+      gen="Sr. ";
   else
-    if(edad.value>30)
-      gen="Sra. ";
-    else
+    if(edad.value<=30)
       gen="Srta. ";
-  div.innerHTML = "<p><b> Hola </b> " + gen + nombre.value + "</p>";
+    else
+      gen="Sra. ";
+  div.innerHTML = "<p><b> Hola </b> " +saludoXHora + " " + gen + nombre.value + "</p>";
 });
